9c9c983dbbc5613cc65766f55cd039e6e945fa3c
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-01-25 12:42:21 -0500

    remove error

--- mapped-types--in-keyword/codes/assignable-to-string.ts
+++
@@ -1,9 +0,0 @@
-import {IsEqual} from  'type-fest'
-
-type AssignableToString<T>  = T extends string ? true : false
-
-type ExpectedToAssiganbleToString =  "name" | "alive" | "foo"
-type assert1 = IsEqual<AssignableToString<ExpectedToAssiganbleToString>, true> // true
-
-type ExpectedToNotAssiganbleToString =  1 | "alive" | "foo"
-type assert2 = IsEqual<AssignableToString<ExpectedToNotAssiganbleToString>, false> // true
